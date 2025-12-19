5a64b8b44d041bbdba3cd549abf9e745e029c0fb
finanovita <finanovita479@gmail.com>
2025-12-19 19:30:08 +0700

    menambahkan DestinasiHome sebagai rute layar utama

---
+++ app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.localrestapi.uicontroller.route
+
+import com.example.localrestapi.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
