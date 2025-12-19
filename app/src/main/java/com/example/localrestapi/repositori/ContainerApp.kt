eaa443408e7b40fec9f6e35be98e251c28c42c64
finanovita <finanovita479@gmail.com>
2025-12-19 19:21:59 +0700

    menambahkan DefaultContainerApp sebagai penyedia repository jaringan dengan konfigurasi Retrofit dan OkHttp

--- app/src/main/java/com/example/localrestapi/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/localrestapi/repositori/ContainerApp.kt
@@ -1,5 +1,43 @@
 package com.example.localrestapi.repositori
 
+import com.example.localrestapi.apiservice.ServiceApiSiswa
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
 interface ContainerApp {
     val repositoryDataSiswa : RepositoryDataSiswa
+}
+
+class DefaultContainerApp : ContainerApp{
+    private val baseurl = "http://10.0.2.2:8080/umyTI/"
+
+    val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+    val klien = OkHttpClient.Builder()
+        .addInterceptor(interceptor = logging)
+        .build()
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(baseurl)
+        .addConverterFactory(
+            Json {
+                ignoreUnknownKeys = true
+                prettyPrint = true
+                isLenient = true
+            }.asConverterFactory(contentType = "application/json".toMediaType())
+        )
+        .client( klien)
+        .build()
+
+    private val retrofitServices : ServiceApiSiswa by lazy {
+        retrofit.create(ServiceApiSiswa::class.java)
+    }
+
+    override val repositoryDataSiswa: RepositoryDataSiswa by lazy {
+        JaringanRepositoryDataSiswa( serviceApiSiswa = retrofitServices) }
 }
