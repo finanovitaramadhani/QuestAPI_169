7e90e8f2a89d69ff1045e629060212559826c579
finanovita <finanovita479@gmail.com>
2025-12-19 19:36:26 +0700

    menambahkan fungsi addSiswa untuk menyimpan data siswa ke API

--- app/src/main/java/com/example/localrestapi/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/EntryViewModel.kt
@@ -34,3 +34,20 @@ class EntryViewModel(
                 isEntryValid = validasiInput(detailSiswa)
             )
     }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            val sip: Response<Void> =
+                repositoryDataSiswa.postDataSiswa(
+                    uiStateSiswa.detailSiswa.toDataSiswa()
+                )
+
+            if (sip.isSuccessful) {
+                println("Sukses Tambah Data : ${sip.message()}")
+            } else {
+                println("Gagal tambah data : ${sip.errorBody()}")
+            }
+        }
+    }
+}
