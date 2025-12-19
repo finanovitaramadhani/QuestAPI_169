340d94c19f14dadab72148ba0d21b8c90590d699
finanovita <finanovita479@gmail.com>
2025-12-19 19:34:49 +0700

    menambahkan EntryViewModel untuk input dan validasi data siswa

---
+++ app/src/main/java/com/example/localrestapi/viewmodel/EntryViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.localrestapi.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.localrestapi.modeldata.DetailSiswa
+import com.example.localrestapi.modeldata.UIStateSiswa
+import com.example.localrestapi.modeldata.toDataSiswa
+import com.example.localrestapi.repositori.RepositoryDataSiswa
+import retrofit2.Response
+
+class EntryViewModel(
+    private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(
+        uiState: DetailSiswa = uiStateSiswa.detailSiswa
+    ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
