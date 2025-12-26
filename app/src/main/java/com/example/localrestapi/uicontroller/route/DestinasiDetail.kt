2a6bdfb016be0af5365f16fa3e6074829d0c38cc
finanovita <finanovita479@gmail.com>
2025-12-26 20:17:48 +0700

    menambahkan DestinasiDetail untuk halaman detail siswa

---
+++ app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiDetail.kt
@@ -0,0 +1,13 @@
+package com.example.localrestapi.uicontroller.route
+
+import com.example.localrestapi.R
+
+object DestinasiDetail: DestinasiNavigasi {
+
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
+
