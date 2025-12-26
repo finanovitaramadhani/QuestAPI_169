16f74047396d2a68143abe02b33ec2aaab68009d
finanovita <finanovita479@gmail.com>
2025-12-26 20:18:22 +0700

    menambahkan DestinasiEdit untuk halaman edit siswa

---
+++ app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiEdit.kt
@@ -0,0 +1,12 @@
+package com.example.localrestapi.uicontroller.route
+
+import com.example.localrestapi.R
+
+object DestinasiEdit : DestinasiNavigasi {
+
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
