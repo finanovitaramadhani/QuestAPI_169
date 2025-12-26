711ed68b20e307f733c15a9813667dd069f4bb18
finanovita <finanovita479@gmail.com>
2025-12-26 20:29:43 +0700

    menambah EditViewModel untuk fitur edit data siswa

---
+++ app/src/main/java/com/example/localrestapi/viewmodel/EditViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.localrestapi.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.localrestapi.modeldata.DetailSiswa
+import com.example.localrestapi.modeldata.UIStateSiswa
+import com.example.localrestapi.modeldata.toDataSiswa
+import com.example.localrestapi.modeldata.toUiStateSiswa
+import com.example.localrestapi.repositori.RepositoryDataSiswa
+import com.example.localrestapi.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+)
+}
