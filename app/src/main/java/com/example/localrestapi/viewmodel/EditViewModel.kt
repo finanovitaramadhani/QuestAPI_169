87766dccd48c222bcb4e3ba7acd458f5fa88be5c
finanovita <finanovita479@gmail.com>
2025-12-26 20:32:32 +0700

    inisialisasi EditViewModel dengan state dan data siswa

--- app/src/main/java/com/example/localrestapi/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/EditViewModel.kt
@@ -18,5 +18,17 @@ import retrofit2.Response
 class EditViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryDataSiswa: RepositoryDataSiswa
-)
-}
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int =
+        checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
